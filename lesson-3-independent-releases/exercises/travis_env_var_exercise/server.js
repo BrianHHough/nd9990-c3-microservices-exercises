c2e4fbdaad9c1776763bcaedafd4cf4f93a27640
Brian Hough <brianhoughmedia@gmail.com>
2021-08-22 16:34:01 -0400

    Update lessons 2-4

--- lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -1,3 +1,5 @@
+// Set a local variable populated by the environment variable $NAME
+const favoriteFood = process.env.FAVORITE_FOOD;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,7 +7,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
